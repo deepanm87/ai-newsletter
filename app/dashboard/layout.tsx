f8f61182175561f45f50462d6ee6a53acfa0ee0d
deepanm87 <65060511+deepanm87@users.noreply.github.com>
2025-11-06 18:34:45 -0500

    added and rendered dashboard header component

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,10 +1,23 @@
+import { auth } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
+import { DashboardHeader } from "@/components/dashboard/dashboard-header"
+
 export default async function Layout({
   children
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const { has } = await auth()
+  const hasPaidPlan = 
+    (await has({ plan: "pro" })) || (await has({ plan: "starter" }))
+
+    if (!hasPaidPlan) {
+      redirect("/pricing")
+    }
+    
   return (
     <div className="min-h-screen flex flex-col">
+      <DashboardHeader />
       <main className="flex-1">
         {children}
       </main>
