0f9f5d6d9d701ea96d496e23a7b2281d37a6edc2
deepanm87 <65060511+deepanm87@users.noreply.github.com>
2025-11-05 22:22:42 -0500

    landing page with components of hero, features, how it works, call to action and badge and card shadcn ui components

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,24 @@
+import { SignedIn, UserButton } from "@clerk/nextjs"
+import { Hero } from "@/components/landing/hero"
+import { Features } from "@/components/landing/features"
+import { HowItWorks } from "@/components/landing/how-it-works"
+import { CTA } from "@/components/landing/cta"
 
 export default function Home() {
   return (
-    <h1>AI Newsletter</h1>
-  );
+    <main className="min-h-screen bg-white dark:bg-black">
+      <Hero />
+      <Features />
+      <HowItWorks />
+
+      <SignedIn>
+        <div className="fixed top-4 right-4">
+          <UserButton />
+        </div>
+      </SignedIn>
+
+     {/* <Pricing /> */}
+      <CTA />
+    </main>
+  )
 }
---
+++ components/landing/buttons/CTAButtons.tsx
@@ -0,0 +1,49 @@
+import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs"
+import { auth } from "@clerk/nextjs/server"
+import { ArrowRight } from "lucide-react"
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
+
+export default async function CTAButtons() {
+  const { has, userId } = await auth()
+  const hasPaidPlan = 
+    (await has({ plan: "pro" })) || (await has({ plan: "starter" }))
+
+    return (
+      <>
+        <SignedOut>
+          <SignInButton mode="modal" forceRedirectUrl="/#pricing">
+            <Button size="lg" className="w-full sm:w-auto cursor-pointer">
+              Get Started <ArrowRight className="ml-2 size-4" />
+            </Button>
+          </SignInButton>
+          <Button
+            asChild
+            size="lg"
+            variant="outline"
+            className="w-full sm:w-auto"
+          >
+            <Link href="#pricing">View Pricing</Link>
+          </Button>
+        </SignedOut>
+
+        {userId && !hasPaidPlan && (
+          <SignedIn>
+            <Button size="lg" className="w-full sm:w-auto" asChild>
+              <Link href="/#pricing" className="flex items-center justify-center">
+                Choose a Plan <ArrowRight className="ml-2 size-4" />
+              </Link>
+            </Button>
+            <Button
+              asChild
+              size="lg"
+              variant="outline"
+              className="w-full sm:w-auto"
+            >
+              <Link href="#pricing">View Pricing</Link>
+            </Button>
+          </SignedIn>
+        )}
+      </>
+    )
+}
